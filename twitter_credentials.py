c162bd930636b313da586e50409f0626cdab0c57
Surya <surya@localhost.localdomain>
2019-06-27 17:54:35 -0500

    Added twitter_credentials.py

---
+++ twitter_credentials.py
@@ -0,0 +1,6 @@
+#Variables that contain the user credentials to access Twitter API
+#You will get these credentials by creating a developer id on twitter
+ACCESS_TOKEN=""
+ACCESS_TOKEN_SECRET=""
+CONSUMER_KEY=""
+CONSUMER_SECRET=""
